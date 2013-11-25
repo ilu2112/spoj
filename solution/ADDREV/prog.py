5d2f082829169fe058d0192b6a1a1df9306597f3
Marcin Skiba <cines91@gmail.com>
2013-11-25 14:18:01 +0100

    Update prog.py

--- solution/ADDREV/prog.py
+++ solution/ADDREV/prog.py
@@ -1,6 +1,6 @@
 a = int(raw_input())
 for _ in range(a):
-	line = raw_input().split(" ")
-	x = int( line[0][::-1].lstrip("0") )
-	y = int( line[1][::-1].lstrip("0") )
-	print str(x+y)[::-1].lstrip("0")
+    line = raw_input().split(" ")
+    x = int( line[0][::-1].lstrip("0") )
+    y = int( line[1][::-1].lstrip("0") )
+    print str(x+y)[::-1].lstrip("0")
